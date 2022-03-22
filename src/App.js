3b3ab5295c5242478adb6a7eb78d04b1d174bcc4
taylor-ann <80003984+taylor-ann@users.noreply.github.com>
2022-03-22 14:33:48 -0400

    added DIctionary file to start build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-      Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded By Taylor Mattscheck</footer>
+      </div>
     </div>
   );
 }
