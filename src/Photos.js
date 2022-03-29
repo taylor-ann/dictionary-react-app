aa7a32d49ec84bde6828c5ed61caaf6035625548
taylor-ann <80003984+taylor-ann@users.noreply.github.com>
2022-03-29 16:32:05 -0400

    another attempted fix

--- src/Photos.js
+++ src/Photos.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "./Photos.css";
 
-export default function Photos(props){
-    if (props.photos){
-    return(
-        <section className="Photos">
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
         <div className="row">
-        {props.photos.map(function (photo, index){
-            return(
-       <div className="col-3" key={index}>
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                    <img src={photo.src.landscape} alt="img" className="img-fluid" />
-                    </a>
-            </div>
+                  <img src={photo.src.landscape} className="img-fluid" alt="pexel" />
+                </a>
+              </div>
             );
-        })}
+          })}
         </div>
-    </section>
+      </section>
     );
-} else {
+  } else {
     return null;
-}
+  }
 }
