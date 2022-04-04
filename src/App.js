d7442e99cf31fbf61d1f02148ed0144224df5462
taylor-ann <80003984+taylor-ann@users.noreply.github.com>
2022-04-04 14:57:10 -0400

    added new styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import Dictionary from "./Dictionary";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          Dictionary
+        </header>
         <main>
-          <Dictionary defaultKeyword="Sunrise" />
+          <Dictionary defaultKeyword="Succulent" />
         </main>
         <footer className="App-footer">
-          <small>Coded By Taylor Mattscheck</small>
+          <small>
+            Coded By{" "}
+            <a
+              href="https://www.linkedin.com/in/taylor-mattscheck-7b1a781a1/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Taylor Mattscheck
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/taylor-ann/dictionary-react-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://dapper-klepon-8157af.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Hosted on Netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
